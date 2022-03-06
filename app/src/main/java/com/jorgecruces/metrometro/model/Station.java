ec9b0a4680c3b50411e7f6fb7663da1b23e42fa3
jaco <jcrucesdeveloper@gmail.com>
2022-03-05 22:00:41 -0300

    feat(structure): first structure project

---
+++ app/src/main/java/com/jorgecruces/metrometro/model/Station.java
@@ -0,0 +1,16 @@
+package com.jorgecruces.metrometro.model;
+
+public class Station {
+
+    private int number;
+    private String name;
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
